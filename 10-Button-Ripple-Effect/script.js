a871b40b6a3ef7915f98ab68c0cdd229bfb8f5e7
Pushpak0000 <bca2005wala@gmail.com>
2025-06-27 22:05:05 +0530

    Add 10th Project

---
+++ 10-Button-Ripple-Effect/script.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('mouseover', function (e) {
+    const x = e.pageX - btn.offsetLeft;
+    const y = e.pageY - btn.offsetTop;
+
+    btn.style.setProperty('--xPos', `${x}px`);
+    btn.style.setProperty('--yPos', `${y}px`);
+  
+});
