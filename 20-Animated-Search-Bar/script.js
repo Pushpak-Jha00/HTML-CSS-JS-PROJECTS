547c48e145fe1a6ed2b64aebfb1c7a25a327f914
Pushpak0000 <bca2005wala@gmail.com>
2025-07-09 14:49:03 +0530

    Add 20th Project

---
+++ 20-Animated-Search-Bar/script.js
@@ -0,0 +1,7 @@
+const searchBarContainerEl = document.querySelector('.search-bar-container');
+
+const magnifireEl = document.querySelector('.magnifire');
+
+magnifireEl.addEventListener("click" , ()=>{
+  searchBarContainerEl.classList.toggle('active')
+})
